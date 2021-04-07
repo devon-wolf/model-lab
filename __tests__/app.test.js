89926a221146a7a341265a1a912a8761cc9a60aa
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-07 09:55:17 -0700

    Add seed users and failing test for get route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,31 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const testUser1 = {
+  username: 'not_cher',
+  email: 'not@cher.com',
+  isContributor: true
+};
+
+const testUser2 = {
+  username: 'meddler',
+  email: 'in@your.business',
+  isContributor: false
+};
+
 describe('model-lab routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  beforeEach(async () => {
+    await request(app)
+      .post('/api/v1/users')
+      .send(testUser1);
+    
+    await request(app)
+      .post('/api/v1/users')
+      .send(testUser2);
   });
 
   it('adds a user to the database', async () => {
@@ -20,8 +42,22 @@ describe('model-lab routes', () => {
       .send(newUser);
 
       expect(response.body).toEqual({
-      id: '1',
+      id: '3',
       ...newUser
     });
+  });
+
+  it('gets all users from the database', async () => {
+    const response = await request(app)
+      .get('/api/v1/users');
+
+    expect(response.body).toEqual([{
+      id: '1',
+      ...testUser1
+    },
+    {
+      id: '2',
+      ...testUser2
+    }]);
   });
 });
